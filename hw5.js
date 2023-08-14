516235f74dbb0c03c8322b5455181eb5a9f8dbbb
Sergei <124916772+aash3r@users.noreply.github.com>
2023-08-14 11:49:54 +0300

    Delete hw5.js

--- hw5.js
+++
@@ -1,6 +0,0 @@
-const changebgColor = () => {
-    const inputColor = document.getElementById ('color').value; 
-    const body = document.getElementById ('bgColor')
-    body.style.backgroundColor = inputColor
-    if (body.style.color === '') console.log ('wrong color')
-}
